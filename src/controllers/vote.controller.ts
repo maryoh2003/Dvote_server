93ae92d2b0763ae3b4cb26a442356cb23a67a7b3
Mary Oh <maryoh2002@gmail.com>
2020-09-12 23:19:14 +0900

    [ADD] modifyVote

--- src/controllers/vote.controller.ts
+++ src/controllers/vote.controller.ts
@@ -5,6 +5,7 @@ import CustomError from '@lib/errors/customError';
 import VoteService from "@service/vote.service";
 import { Request, Response, NextFunction } from 'express';
 import AuthRequest from "types/AuthRequest";
+import getNumberParam from "@lib/util/getNumberParam";
 
 
 @Service()
@@ -30,6 +31,29 @@ export default class VoteController {
 
       res.status(200).json({
         message: '설문 생성 성공',
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  /**
+   * @description 설문 수정
+   */
+  public modifyVote = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const idx = getNumberParam(req.params);
+      const { body } = req;
+      const data = new VoteRequest(body);
+
+      if (!await data.validate()) {
+        throw new CustomError(errors.WrongRequest);
+      }
+
+      await this.voteService.modifyVote(idx, data);
+
+      res.status(200).json({
+        message: '설문 수정 성공',
       });
     } catch (err) {
       next(err);
